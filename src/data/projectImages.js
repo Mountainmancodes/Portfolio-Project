9c9270767da4b05ec6373ca46f67e4a1f1946928
Mountainmancodes <Colorado.JHernandez@gmail.com>
2024-09-02 00:48:20 -0600

    adding place holders for future images.

--- src/data/projectImages.js
+++ src/data/projectImages.js
@@ -1,7 +1,20 @@
 const projectImages = {
     'Blog-Project': './assets/images/blogsproject.png',
     'C12-Employee-Tracker': '/images/employee-tracker.png',
-    // Add more projects and their corresponding image paths here
+    'C13-ECommerce':'',
+    'C14-TechBlog':'',
+    'Challenge-9-README-generator':'',
+    'first-day-repo':'',
+    'Github-Practice-Repo':'',
+    'Horiseone-Project':'',
+    'M10C-SVG-Logo-Maker':'',
+    'M11-Note-Taker':'',
+    'MMC-Payroll-Tracker':'',
+    'Mountainmancodes':'',
+    'Musical-Board':'',
+    'Portfolio-Project':'',
+    'prework-study-guide':'',
+    'Weathermanknows':'',
   };
   
   export default projectImages;
